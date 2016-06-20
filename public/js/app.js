0f0fdcc6f97762a4e0770f5257b96c5cb94aa6a5
jfkbeatkid <hernadezrigoberto@gmail.com>
2016-06-20 19:55:56 -0400

    created seconds countdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-console.log('my java script is working');
+$(document).ready(function() {
+  var $start = $('#start');
+  var $minutes = $('#minutes');
+  var $seconds = $('#seconds');
+
+  $start.on('click',startCountdown);
+
+  function startCountdown(){
+    setInterval(function(){
+      var secondsVal = +$seconds.text();
+      // plus sign makes it behave like a number
+        if(secondsVal === 0) {
+          $seconds.text(59);
+        } else {
+          $seconds.text(secondsVal - 1);
+        }
+    }, 1000);
+  }
+});
